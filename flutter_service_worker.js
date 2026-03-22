8f26730a180d9fd17e4869953924fbaea1f812cf
Eddie <walkmandede@gmail.com>
2026-03-22 21:34:16 +0700

    deploy 1.0.6prod

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d423eb60b12a1a73d57d71a3b532fb5f",
+const RESOURCES = {"flutter_bootstrap.js": "9af7a2e3f169456eafff226f4596d2b0",
 "version.json": "7bf4092c28ae64bba0e52703e4d68e0b",
 "index.html": "c26162cf7ea3b9f88dd230b965678bfa",
 "/": "c26162cf7ea3b9f88dd230b965678bfa",
-"main.dart.js": "e76ced6b5b8a12addcb58299f39d27b6",
+"main.dart.js": "444e125c2b3f4a2c8ce2a7389fdef14b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "0971a29f6e28edcb7470e9a78bce22e4",
 "main.dart.mjs": "6b7dc94919c28ae4f4ed0c71c78ab87f",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "d423eb60b12a1a73d57d71a3b532fb5f",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "98fa5fcf569ffc8629415305dae8ae0a",
-"main.dart.wasm": "9099166c26de5473efe624a41daa1b9b",
+"main.dart.wasm": "c67e1054c53bc1209ea06bf19c434150",
 "assets/AssetManifest.json": "d5b8bc337beed62c43032a2b772a2b5a",
 "assets/NOTICES": "2de5256b76dc906629790cbe97ea48f3",
 "assets/FontManifest.json": "4f08a60ed482dd72584fe80d1b650408",
